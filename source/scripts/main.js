2d3095d89f6337fe06582fa56c155dc02e136a4d
GTM35 <035guuh@gmail.com>
2024-01-30 14:06:18 -0300

    Finalizando área de curiosidades

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -4,8 +4,33 @@ btnMenuMobile.addEventListener("click", () => {
   menuMobile();
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+  cardAnimation();
+});
+
 function menuMobile() {
   const menuMobile = document.querySelector(".header__menu");
 
   menuMobile.classList.toggle("header__menu--is-active");
+}
+
+function cardAnimation() {
+  const icon = document.querySelectorAll(".curiosity__header__icon img");
+  icon.forEach((element) => {
+    element.addEventListener("click", (el) => {
+      el.target.parentNode.parentNode.nextElementSibling.classList.toggle(
+        "curiosity__description--is-active"
+      );
+
+      if (
+        el.target.parentNode.parentNode.nextElementSibling.classList.contains(
+          "curiosity__description--is-active"
+        )
+      ) {
+        el.target.setAttribute("src", "./source/images/icon-.svg");
+      } else {
+        el.target.setAttribute("src", "./source/images/icon+.svg");
+      }
+    });
+  });
 }
