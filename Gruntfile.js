a4910082a4b5a37e2895e3ca91aaa7d9e65515b4
GTM35 <035guuh@gmail.com>
2024-01-30 17:47:59 -0300

    Finalizando ajustes

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,20 +3,69 @@ module.exports = (grunt) => {
     pkg: grunt.file.readJSON("package.json"),
     less: {
       development: {
-        files: { "./source/styles/main.css": "./source/styles/main.less" },
+        files: {
+          "./source/assets/styles/main.css": "./source/assets/styles/main.less",
+        },
+      },
+      production: {
+        options: {
+          compress: true,
+        },
+        files: {
+          "./dist/styles/main.min.css": "./source/assets/styles/main.less",
+        },
       },
     },
 
     watch: {
       less: {
-        files: ["./source/styles/*.less"],
+        files: ["./source/assets/styles/*.less"],
         tasks: ["less:development"],
+      },
+    },
+
+    uglify: {
+      options: {
+        mangle: false,
+      },
+      my_target: {
+        files: {
+          "./dist/js/main.min.js": "./source/assets/scripts/main.js",
+        },
+      },
+    },
+
+    copy: {
+      main: {
+        files: [
+          {
+            expand: true,
+            cwd: "./source/assets/images/",
+            src: ["**"],
+            dest: "./dist/images/",
+          },
+          {
+            expand: true,
+            cwd: "./source/assets/videos/",
+            src: ["**"],
+            dest: "./dist/videos/",
+          },
+          {
+            expand: true,
+            cwd: "./source/assets/fonts/",
+            src: ["**"],
+            dest: "./dist/fonts/",
+          },
+        ],
       },
     },
   });
 
   grunt.loadNpmTasks("grunt-contrib-less");
   grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-copy");
 
   grunt.registerTask("default", ["watch"]);
+  grunt.registerTask("build", ["less:production", "uglify", "copy"]);
 };
